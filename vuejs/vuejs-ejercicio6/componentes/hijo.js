7abb1a7d3f30c26172defaa76dfbb88a959c53e8
Joaquín Vilchez <joaquinvilchez95@gmail.com>
2021-06-23 12:44:18 -0300

    Envio de datos a traves de props de hijo a padre. -No me sale enviar el numero del hijo al numero padre

--- vuejs/vuejs-ejercicio6/componentes/hijo.js
+++ vuejs/vuejs-ejercicio6/componentes/hijo.js
@@ -1,9 +1,21 @@
 Vue.component('hijo', {
-    template: //html
-    `
+    template:
+        `
         <div class="py-5 bg-success">
             <h4>Componente hijo: {{numero}}</h4>
+            <h4>Nombre: {{nombre}}</h4>
+            <button class="btn btn-light" @click="numero++">+</button>
+            <button class="btn btn-dark" @click="numero--">-</button>
         </div>
     `,
-    props: ['numero']
+    props: ['numero'],
+    data() {
+        return {
+            nombre: 'Juan'
+        }
+    },
+    mounted(){ //Se ejecuta una vez que el DOM ya esta listo
+        this.$emit('nombreHijo', this.nombre) //Emite un evento
+        this.$emit('numeroHijo', this.numero)
+    }
 })
--- vuejs/vuejs-ejercicio6/componentes/padre.js
+++ vuejs/vuejs-ejercicio6/componentes/padre.js
@@ -1,3 +1,8 @@
+/*Este componente recibe a traves del evento 
+@nombreHijo creado en el componente hijo.js 
+el nombre asignado. Es necesario asignarlo 
+en una propiedad data*/
+
 Vue.component('padre',{
     template: //html
     `
@@ -5,12 +10,14 @@ Vue.component('padre',{
             <h2>Componente padre: {{numeroPadre}}</h2>
             <button class="btn btn-success my-1" @click="numeroPadre++">+</button>
             <button class="btn btn-danger my-1" @click="numeroPadre--">-</button>
-            <hijo :numero="numeroPadre"></hijo>
+            <h3>Nombre padre: {{nombrePadre}}</h3> 
+            <hijo :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
         </div>
     `,
     data(){
         return{
-            numeroPadre: 0
+            numeroPadre: 0,
+            nombrePadre: ''
         }
     }
 })
